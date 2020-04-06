8d1926e81cd0778b3986ce2177e2d733b69d2682
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-04-06 16:55:37 +1000

    Add login and logout buttons, hide edit and delete for logged out users

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Discuss.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Discuss.Plugs.SetUser
   end
 
   pipeline :api do
@@ -24,6 +25,7 @@ defmodule Discuss.Router do
   scope "/auth", Discuss do
     pipe_through :browser
 
+    get "/signout", AuthController, :signout
     get "/:provider", AuthController, :request
     get "/:provider/callback", AuthController, :callback
   end
